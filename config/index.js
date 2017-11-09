d29bfe7b066478d021bc865e3a274c1f4bf6882b
Wizard67 <wizard67.hjn@gmail.com>
2017-11-09 09:48:07 +0800

    [modify]修改 webpakc 生成文件目录结构

--- config/index.js
+++ config/index.js
@@ -7,10 +7,10 @@ const path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../_site/index.html'),
-    assetsRoot: path.resolve(__dirname, '../_site'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    index: path.resolve(__dirname, '../index.html'),
+    assetsRoot: path.resolve(__dirname, '../assets'),
+    assetsSubDirectory: '',
+    assetsPublicPath: '/assets',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
@@ -28,7 +28,7 @@ module.exports = {
     env: require('./dev.env'),
     port: process.env.PORT || 8080,
     autoOpenBrowser: true,
-    assetsSubDirectory: 'static',
+    assetsSubDirectory: '',
     assetsPublicPath: '/',
     proxyTable: {},
     // CSS Sourcemaps off by default because relative paths are "buggy"
