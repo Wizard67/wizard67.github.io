fc1c3b873432f8e7a27301e2915c1a637ada9291
Wizard67 <wizard67.hjn@gmail.com>
2017-11-10 14:06:29 +0800

    [format]代码排版

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <div id="app">
-
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  created(){
-    console.log(this.$router)
-  }
+    name: 'app'
 }
 </script>
 
 <style src="@/assets/css/base.scss" lang="scss"></style>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-* {
-  outline: 1px red solid;
-}
-</style>
