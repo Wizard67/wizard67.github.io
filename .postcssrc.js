0d02b7c8e4d658bb716fb2b2c6315181c17e4051
Wizard67 <wizard67.hjn@gmail.com>
2018-01-19 17:46:08 +0800

    [fixed] 修复 css-mqpacker 插件媒体查询排序错误

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,11 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
+const sortCSSmq = require('sort-css-media-queries');
 
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
     "autoprefixer": {},
     "cssnano": {preset: 'default'},
-    "css-mqpacker": {}
+    "css-mqpacker": {sort: sortCSSmq}
   }
 }
