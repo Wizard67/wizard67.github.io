62784abd8d4e2a7a506f12e4510f0a38ef39be34
Wizard67 <wizard67.hjn@gmail.com>
2018-02-27 17:37:25 +0800

    [storage]

--- src/pages/note/page.vue
+++ src/pages/note/page.vue
@@ -1,5 +1,151 @@
 <template>
-    <article class="content -note" v-html="content"></article>
+    <!-- <article class="content -note" v-html="content"></article> -->
+    <article class="content -note">
+        <h2 id="css">css</h2>
+
+        <dl>
+        <dt>这是一个测试用的定义列表</dt>
+        <dd>
+            <div class="language-js highlighter-rouge"><div class="highlight"><pre class="highlight"><code>  <span class="cm">/**
+        * Does a thing
+        */</span>
+        <span class="kd">function</span> <span class="nx">helloWorld</span><span class="p">(</span><span class="nx">param1</span><span class="p">,</span> <span class="nx">param2</span><span class="p">)</span> <span class="p">{</span>
+            <span class="kd">var</span> <span class="nx">something</span> <span class="o">=</span> <span class="mi">0</span><span class="p">;</span>
+            <span class="c1">// Do something</span>
+            <span class="k">if</span> <span class="p">(</span><span class="mf">2.0</span> <span class="o">%</span> <span class="mi">2</span> <span class="o">==</span> <span class="nx">something</span><span class="p">)</span> <span class="p">{</span>
+                <span class="nx">console</span><span class="p">.</span><span class="nx">log</span><span class="p">(</span><span class="s1">'Hello, world!'</span><span class="p">);</span>
+            <span class="p">}</span> <span class="k">else</span> <span class="p">{</span>
+                <span class="k">return</span> <span class="kc">null</span><span class="p">;</span>
+            <span class="p">}</span>
+        <span class="p">}</span>
+        </code></pre></div>    </div>
+        </dd>
+        <dd><a href="//baidu.com">外链</a></dd>
+        </dl>
+
+        <dl>
+        <dt>这是一个测试用的定义列表</dt>
+        <dd>
+            <div class="language-js highlighter-rouge"><div class="highlight"><pre class="highlight"><code>  <span class="cm">/**
+        * Does a thing
+        */</span>
+        </code></pre></div>    </div>
+        </dd>
+        <dd><a href="//baidu.com">外链</a></dd>
+        </dl>
+
+        <dl>
+        <dt>这是一个测试用的定义列表</dt>
+        <dd>你好</dd>
+        <dd><a href="//baidu.com">外链</a></dd>
+        </dl>
+
+        <dl>
+        <dt>这是一个测试用的定义列表</dt>
+        <dd>列表项一</dd>
+        <dd>列表项二</dd>
+        <dd>
+            <div class="language-css highlighter-rouge"><div class="highlight"><pre class="highlight"><code>  <span class="k">@import</span> <span class="n">extra</span><span class="p">.</span><span class="n">css</span><span class="p">;</span>
+        <span class="o">*</span> <span class="p">{</span>
+            <span class="nl">margin</span><span class="p">:</span> <span class="m">0</span><span class="p">;</span>
+        <span class="p">}</span>
+        <span class="nt">ul</span> <span class="nt">li</span> <span class="p">{</span>
+            <span class="nl">list-style</span><span class="p">:</span> <span class="n">linear</span><span class="p">;</span>
+            <span class="nl">margin</span><span class="p">:</span> <span class="m">2em</span> <span class="m">20%</span> <span class="m">15px</span> <span class="m">0</span><span class="p">;</span>
+        <span class="p">}</span>
+        <span class="nf">#content</span> <span class="p">{</span>
+            <span class="nl">-webkit-font-smoothing</span><span class="p">:</span> <span class="n">antialiased</span><span class="p">;</span>
+            <span class="nl">background</span><span class="p">:</span> <span class="sx">url(img/gradient.png)</span><span class="p">;</span>
+            <span class="nl">background</span><span class="p">:</span> <span class="n">linear-gradient</span><span class="p">(</span><span class="n">to</span> <span class="nb">bottom</span><span class="p">,</span> <span class="no">red</span><span class="p">,</span> <span class="n">rgba</span><span class="p">(</span><span class="m">255</span><span class="p">,</span><span class="m">0</span><span class="p">,</span><span class="m">0</span><span class="p">,</span><span class="m">0</span><span class="p">))</span>
+        <span class="p">}</span>
+        </code></pre></div>    </div>
+        </dd>
+        <dd><a href="//baidu.com">外链</a></dd>
+        </dl>
+
+        <dl>
+        <dt>这是一个测试用的定义列表</dt>
+        <dd>列表项一</dd>
+        <dd>列表项二</dd>
+        <dd><a href="//baidu.com">外链</a></dd>
+        </dl>
+
+        <dl>
+        <dt>这是一个测试用的定义列表</dt>
+        <dd>列表项一</dd>
+        <dd>列表项二</dd>
+        <dd><a href="//baidu.com">外链</a></dd>
+        </dl>
+
+        <dl>
+        <dt>这是一个测试用的定义列表</dt>
+        <dd>列表项一</dd>
+        <dd>列表项二</dd>
+        <dd><a href="//baidu.com">外链</a></dd>
+        </dl>
+
+        <h2 id="js">js</h2>
+
+        <dl>
+        <dt>JS 属性</dt>
+        <dd>对象</dd>
+        <dd>属性一</dd>
+        <dd>属性二</dd>
+        <dd><a href="//nihao.cc">参考</a></dd>
+        </dl>
+
+        <h2 id="html">html</h2>
+        <dl>
+        <dt>html</dt>
+        <dd>对象</dd>
+        <dd>属性一</dd>
+        <dd>
+            <div class="language-html highlighter-rouge"><div class="highlight"><pre class="highlight"><code>  <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"CJ_dialog"</span> <span class="na">data-js-dialog</span><span class="nt">&gt;</span>
+            <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"CJ_dialog__main"</span> <span class="na">style=</span><span class="s">"background: url(./img/d1.png); width: 688px; height: 608px;"</span><span class="nt">&gt;</span>
+                <span class="nt">&lt;img</span> <span class="na">class=</span><span class="s">"CJ_detail"</span> <span class="na">src=</span><span class="s">"./img/d1-c.png"</span> <span class="na">style=</span><span class="s">"top: 70px; right: 30px; cursor: pointer;"</span> <span class="na">data-js-dialogClose</span><span class="nt">&gt;</span>
+                <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"CJ_detail"</span> <span class="na">style=</span><span class="s">"width: 500px;top: 160px; right: 70px;"</span><span class="nt">&gt;</span>
+                    <span class="nt">&lt;p</span> <span class="na">class=</span><span class="s">"CJ_dialog__title"</span><span class="nt">&gt;</span>恭喜您获得<span class="nt">&lt;/p&gt;</span>
+                <span class="nt">&lt;/div&gt;</span>
+                <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"CJ_detail"</span> <span class="na">style=</span><span class="s">"width: 500px;top: 360px; right: 70px;"</span><span class="nt">&gt;</span>
+                    <span class="nt">&lt;p</span> <span class="na">class=</span><span class="s">"CJ_dialog__summary"</span><span class="nt">&gt;</span>5000元奖励金<span class="nt">&lt;/p&gt;</span>
+                <span class="nt">&lt;/div&gt;</span>
+                <span class="nt">&lt;div</span> <span class="na">class=</span><span class="s">"CJ_detail"</span> <span class="na">style=</span><span class="s">"width: 500px;top: 440px; right: 70px; text-align: center;"</span><span class="nt">&gt;</span>
+                    <span class="nt">&lt;a</span> <span class="na">href=</span><span class="s">""</span> <span class="na">class=</span><span class="s">"CJ_button -dialog"</span> <span class="na">style=</span><span class="s">"width: 164px; height: 51px;background: url(./img/d1-btn.png);"</span> <span class="na">data-js-dialogClose</span><span class="nt">&gt;</span>确定<span class="nt">&lt;/a&gt;</span>
+                <span class="nt">&lt;/div&gt;</span>
+            <span class="nt">&lt;/div&gt;</span>
+        <span class="nt">&lt;/div&gt;</span>
+        </code></pre></div>    </div>
+        </dd>
+        <dd><a href="//nihao.cc">参考</a></dd>
+        </dl>
+
+        <h2 id="vue">vue</h2>
+        <dl>
+        <dt>vue</dt>
+        <dd>
+            <div class="language-vue highlighter-rouge"><div class="highlight"><pre class="highlight"><code>  <span class="nt">&lt;</span><span class="k">template</span><span class="nt">&gt;</span>
+            <span class="nt">&lt;article</span> <span class="na">class=</span><span class="s">"content -note"</span> <span class="na">v-html=</span><span class="s">"content"</span><span class="nt">&gt;&lt;/article&gt;</span>
+        <span class="nt">&lt;/</span><span class="k">template</span><span class="nt">&gt;</span>
+
+        <span class="nt">&lt;</span><span class="k">script</span><span class="nt">&gt;</span>
+        <span class="k">import</span> <span class="nx">GetContent</span> <span class="k">from</span> <span class="s1">'../mixin'</span>
+
+        <span class="kd">const</span> <span class="nx">namespace</span> <span class="o">=</span> <span class="s1">'note'</span>
+
+        <span class="k">export</span> <span class="k">default</span> <span class="p">{</span>
+            <span class="na">path</span><span class="p">:</span> <span class="s2">`/</span><span class="p">${</span><span class="nx">namespace</span><span class="p">}</span><span class="s2">/:pre/:title`</span><span class="p">,</span>
+            <span class="na">name</span><span class="p">:</span> <span class="s2">`</span><span class="p">${</span><span class="nx">namespace</span><span class="p">}</span><span class="s2">`</span><span class="p">,</span>
+            <span class="na">mixins</span><span class="p">:</span> <span class="p">[</span> <span class="nx">GetContent</span><span class="p">(</span><span class="s2">`</span><span class="p">${</span><span class="nx">namespace</span><span class="p">}</span><span class="s2">`</span><span class="p">)</span> <span class="p">],</span>
+            <span class="nx">data</span><span class="p">()</span> <span class="p">{</span>
+                <span class="k">return</span> <span class="p">{}</span>
+            <span class="p">}</span>
+        <span class="p">}</span>
+        <span class="nt">&lt;/</span><span class="k">script</span><span class="nt">&gt;</span>
+        </code></pre></div>    </div>
+        </dd>
+        </dl>
+
+    </article>
 </template>
 
 <script>
@@ -17,8 +163,10 @@ export default {
     },
     mounted() {
         const grid = document.getElementsByTagName('article')[0]
+        console.log(grid)
         const msnry = new Masonry( grid, {
-            columnWidth: 20
+            columnWidth: 'dl',
+            percentPosition: true
         })
     },
 }
