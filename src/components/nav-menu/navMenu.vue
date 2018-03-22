cfc988a5820a8f265b5877dcbd393be17d7c70d9
Wizard67 <wizard67.hjn@gmail.com>
2018-03-22 21:35:29 +0800

    [modify] 重写 NavMenu 组件

--- src/components/nav-menu/navMenu.vue
+++ src/components/nav-menu/navMenu.vue
@@ -1,33 +1,44 @@
 <template>
     <nav class="nav-bar">
-        <!-- menu -->
-        <ul class="nav-bar__menu">
-        <template v-for="(i, k) in nav">
-            <li class="nav-bar__item" :key="k">
-                <a :href="`#${i.column}`" @click="getIndex(i.column)">{{i.column}}</a>
-            </li>
-        </template>
-        </ul>
+        
+        <template v-if="nav">
+            <!-- menu -->
+            <ul class="nav-bar__menu">
+                <li class="nav-bar__item"
+                    v-for="(i, k) in nav"
+                    :key="k" >
+                    <a :href="`#${i.column}`"
+                        @click="getIndex(i.column)" >
+                        {{i.column}}
+                    </a>
+                </li>
+            </ul>
 
-        <!-- category -->
-        <div class="nav-bar__category">
-        <template v-for="(i, k) in categorys">
-        <section :key="k + i.category">
-            <p class="nav-bar__type">
-                {{ i.category }}<br>
-            </p>
+            <!-- category -->
+            <div class="nav-bar__category">
+            <template v-for="(i, k) in categorys">
+            <section :key="k + i.category">
+                <p class="nav-bar__type">
+                    {{ i.category }}<br>
+                </p>
 
-            <template v-for="(i1, k1) in i.items">
-                <router-link class="nav-bar__title"
-                             :key="k1"
-                             :to="{name: index.toLowerCase(), params: { pre: i['category'], title: i1['title'] }}"
-                             :title="i1['title']">{{i1['title']}}
-                </router-link>
+                <template v-for="(i1, k1) in i.items">
+                    <router-link class="nav-bar__title"
+                                :key="k1"
+                                :to="{name: index.toLowerCase(), params: { pre: i['category'], title: i1['title'] }}"
+                                :title="i1['title']">{{i1['title']}}
+                    </router-link>
+                </template>
+
+            </section>
             </template>
-
-        </section>
+            </div>
         </template>
-        </div>
+
+        <template v-else>
+            <slot name="empty"></slot>
+        </template>
+
     </nav>
 </template>
 
@@ -37,14 +48,16 @@ const hammer = new Hammer(document)
 
 export default {
     name: 'navMenu',
-    props: [
-        'author',
-        'nav'
-    ],
+    props: [ 'nav' ],
     data() {
         return {
-            index: this.nav[0].column
+            index: this.nav[0].column,
         }
+    },
+    computed: {
+        categorys() {
+            return this.nav.filter(item => item.column === this.index)[0].categorys
+        },
     },
     methods: {
         getIndex(index){
@@ -52,16 +65,11 @@ export default {
         },
         handleClickOutside(e) {
             if ( !this.$el.contains(e.target) ) {
-                e.target.alt !== 'avatar' && this.$store.commit('toggleFocus')
+                e.target.alt !== 'avatar' && this.$emit('outsideClick')
             } else {
-                e.target.nodeName !== 'A' && this.$store.commit('toggleFocus')
+                e.target.nodeName !== 'A' && this.$emit('outsideClick')
             }
-        }
-    },
-    computed: {
-        categorys() {
-            return this.nav.filter(item => item.column === this.index)[0].categorys
-        }
+        },
     },
     mounted() {
         hammer.on('tap', this.handleClickOutside)
