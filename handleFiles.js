82c0c2b0ce5f05543e91f06dfab8591112a337e5
Wizard67 <wizard67.hjn@gmail.com>
2018-03-31 21:16:05 +0800

    feature: 自动处理文件结构

---
+++ handleFiles.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const fs = require("fs-extra")
+const _ = require('lodash')
+const filename = path.join(__dirname, "./dist")
+
+const keepFiles = [
+  '.git',
+  '_articles',
+  '_notes',
+  '_site',
+  'api',
+  'dist',
+  'node_modules',
+  'public',
+  'src',
+  '_config.yml',
+  '.gitignore',
+  'Gemfile',
+  'Gemfile.lock',
+  'handleFiles.js',
+  'package-lock.json',
+  'package.json',
+  'README.md',
+  'vue.config.js',
+]
+
+/* 
+ * remove files that not keep
+ */
+const currentFiles = fs.readdirSync(__dirname)
+
+_.difference(currentFiles, keepFiles).forEach((file)=>{
+
+  fs.removeSync( path.join(__dirname, file) )
+
+})
+return
+/* 
+ *  move files to root-path
+ */
+if (fs.existsSync(filename)) {
+  try {
+    fs.moveSync(filename, __dirname)
+
+    console.log("success!")
+  } catch (err) {
+    console.error(err)
+  }
+} else {
+    console.error("no such filename")
+}
