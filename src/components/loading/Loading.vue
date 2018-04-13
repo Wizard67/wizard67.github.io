8b34518c9420d1fa94780202a350a9c2dde1c991
Wizard67 <wizard67.hjn@gmail.com>
2018-04-13 13:53:47 +0800

    modify: 抽离 Loading 组件

---
+++ src/components/loading/Loading.vue
@@ -0,0 +1,14 @@
+<template functional>
+    <div class="loading-wrap">
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src="./style.scss" lang="scss"></style>
