b75ca1c411b3afd61b5284cef523c12357074943
Wizard67 <wizard67.hjn@gmail.com>
2018-04-23 17:09:27 +0800

    modify: masonry 配置修改为 以 ul 标签作为参照

--- src/views/note/page.vue
+++ src/views/note/page.vue
@@ -1,41 +1,46 @@
 <template>
-    <article class="content -note" v-if="content" v-html="content" />
-    <Loading v-else />
+  <article class="content -note" v-if="content" v-html="content" />
+  <Loading v-else />
 </template>
 
 <script>
-import Masonry from 'masonry-layout'
-import Loading from '@/components/loading'
+import Masonry from "masonry-layout"
+import Loading from "@/components/loading"
 
 let masonry
 
 export default {
-    path: '/notes/:pre/:title',
-    name: 'notes',
-    components: {
-        Loading
-    },
-    computed: {
-        content() {
-            return this.$store.state['notes'].content
-        }
-    },
-    mounted() {
-        this.$store.dispatch('notes/getContent', this.$route)
-    },
-    watch: {
-        content(){
-            this.$nextTick(()=>{
-                masonry = new Masonry( this.$el, {
-                    columnWidth: 'dl',
-                    percentPosition: true
-                } )
-            })
-        }
-    },
-    destroyed() {
-        masonry.destroy()
+  path: "/notes/:pre/:title",
+  name: "notes",
+  components: {
+    Loading
+  },
+  computed: {
+    content() {
+      return this.$store.state["notes"].content
     }
+  },
+  mounted() {
+    this.$store.dispatch("notes/getContent", this.$route)
+  },
+  watch: {
+    content() {
+      this.$nextTick(() => {
+        masonry = this.$el.contains(this.$el.getElementsByTagName("ul")[0])
+          ? new Masonry(this.$el, {
+              columnWidth: "ul",
+              horizontalOrder: true,
+              percentPosition: true
+            })
+          : {
+              destroy() {}  // 防止销毁时为空对象
+            }
+      })
+    }
+  },
+  destroyed() {
+    masonry.destroy()
+  }
 }
 </script>
 
